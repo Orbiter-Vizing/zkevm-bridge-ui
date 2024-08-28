418b92b316daddf77b233464b81ee66e48d3408b
Mitten338 <mitten338@gmail.com>
2024-08-28 16:09:41 +0800

    feat: add chain config and token config for prod

--- src/assets/ethereum-erc20-tokens.ts
+++ src/assets/ethereum-erc20-tokens.ts
@@ -138,23 +138,140 @@ export const EthereumErc20TokensConfig: EthereumErc20TokensConfigInterface = {
     },
   ],
   production: [
+    // Ethereum USDT
     {
       address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
       chainId: 1,
       decimals: 6,
-      logoURI:
-        "https://assets-cdn.trustwallet.com/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
+      logoURI: IconUSDT,
       name: "Tether USD",
       symbol: "USDT",
     },
+    // Ethereum USDC
     {
       address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
       chainId: 1,
       decimals: 6,
-      logoURI:
-        "https://assets-cdn.trustwallet.com/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
+      logoURI: IconUSDC,
       name: "USD Coin",
       symbol: "USDC",
+    },
+    // Ethereum ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 1,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // Vizing USDT
+    {
+      address: "0x1d1A26a8A05D22b5C7FF2bC4df21bA3d3E669bD9",
+      chainId: 28518,
+      decimals: 6,
+      logoURI: IconUSDT,
+      name: "Tether USD",
+      symbol: "USDT",
+    },
+    // Vizing USDC
+    {
+      address: "0x7b54dfc61b6e2A440FC2Abf31f01e98831C49303",
+      chainId: 28518,
+      decimals: 6,
+      logoURI: IconUSDC,
+      name: "USD Coin",
+      symbol: "USDC",
+    },
+    // Vizing ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 28518,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // Arbitrum ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 42161,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // Optimism ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 10,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // Base ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 8453,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // Taiko ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 167000,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // Linea ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 59144,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // Scroll ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 534352,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // Blast ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 81457,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // BOB ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 60808,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    // PolygonzkEVM ETH
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 1101,
+      decimals: 18,
+      logoURI: IconETH,
+      name: "Ether",
+      symbol: "ETH",
     },
   ],
   test: [
@@ -177,14 +294,14 @@ export const EthereumErc20TokensConfig: EthereumErc20TokensConfigInterface = {
       symbol: "USDC",
     },
     // Ethereum Orbit
-    {
-      address: "0x4c668404e961ba1b76abfa923f5490322f72be85",
-      chainId: 11155111,
-      decimals: 9,
-      logoURI: "https://pbs.twimg.com/profile_images/1797891012875956224/y1DBOGtV_400x400.jpg",
-      name: "Orbit Coin",
-      symbol: "Orbit",
-    },
+    // {
+    //   address: "0x4c668404e961ba1b76abfa923f5490322f72be85",
+    //   chainId: 11155111,
+    //   decimals: 9,
+    //   logoURI: "https://pbs.twimg.com/profile_images/1797891012875956224/y1DBOGtV_400x400.jpg",
+    //   name: "Orbit Coin",
+    //   symbol: "Orbit",
+    // },
     // Ethereum ETH
     {
       address: "0x0000000000000000000000000000000000000000",
@@ -195,14 +312,14 @@ export const EthereumErc20TokensConfig: EthereumErc20TokensConfigInterface = {
       symbol: "ETH",
     },
     // Vizing Oribt
-    {
-      address: "0x130e103c3042336d957f15020e5a87e59bd3f8a7",
-      chainId: 28516,
-      decimals: 9,
-      logoURI: "https://pbs.twimg.com/profile_images/1797891012875956224/y1DBOGtV_400x400.jpg",
-      name: "Orbit Coin",
-      symbol: "Orbit",
-    },
+    // {
+    //   address: "0x130e103c3042336d957f15020e5a87e59bd3f8a7",
+    //   chainId: 28516,
+    //   decimals: 9,
+    //   logoURI: "https://pbs.twimg.com/profile_images/1797891012875956224/y1DBOGtV_400x400.jpg",
+    //   name: "Orbit Coin",
+    //   symbol: "Orbit",
+    // },
     // Vizing ETH
     {
       address: "0x0000000000000000000000000000000000000000",
