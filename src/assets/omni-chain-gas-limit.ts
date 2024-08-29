25c63ab0af8ae8bfdb9ffe1bd95b0a63eb06f958
Mitten338 <mitten338@gmail.com>
2024-08-29 12:03:54 +0800

    feat: update OmniChainGasLimitConfig for prod

--- src/assets/omni-chain-gas-limit.ts
+++ src/assets/omni-chain-gas-limit.ts
@@ -11,7 +11,72 @@ interface GasLimitConfig {
 export type EnvString = "test" | "production";
 
 export const OmniChainGasLimitConfig: OmniChainGasLimitConfigInterface = {
-  production: [],
+  production: [
+    {
+      gasLimit: 50000,
+      id: "42161-28518", // arbitrum - vizing
+    },
+    {
+      gasLimit: 500000,
+      id: "28518-42161", // vizing - arbitrum
+    },
+    {
+      gasLimit: 50000,
+      id: "10-28518", // optimism - vizing
+    },
+    {
+      gasLimit: 500000,
+      id: "28518-10", // vizing - optimism
+    },
+    {
+      gasLimit: 50000,
+      id: "1101-28518", // polygon - vizing
+    },
+    {
+      gasLimit: 500000,
+      id: "28518-1101", // vizing - polygon
+    },
+    {
+      gasLimit: 50000,
+      id: "8453-28518", // base - vizing
+    },
+    {
+      gasLimit: 500000,
+      id: "28518-8453", // vizing - base
+    },
+    {
+      gasLimit: 50000,
+      id: "167000-28518", // taiko - vizing
+    },
+    {
+      gasLimit: 500000,
+      id: "28518-167000", // vizing - taiko
+    },
+    {
+      gasLimit: 50000,
+      id: "59144-28518", // linea - vizing
+    },
+    {
+      gasLimit: 500000,
+      id: "28518-59144", // vizing - linea
+    },
+    {
+      gasLimit: 50000,
+      id: "81457-28518", // blast - vizing
+    },
+    {
+      gasLimit: 500000,
+      id: "28518-81457", // vizing - blast
+    },
+    {
+      gasLimit: 50000,
+      id: "60808-28518", // bob - vizing
+    },
+    {
+      gasLimit: 500000,
+      id: "28518-60808", // vizing - bob
+    },
+  ],
   test: [
     {
       gasLimit: 50000,
